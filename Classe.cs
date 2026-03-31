12e81f93a4aa1f01aa62458fb2735a6488a616f2
AnatoleBoit <anatole.boittiaux@edu.esiee.fr>
2026-03-31 16:12:06 +0200

    Ajout gestion moyennes

---
+++ Classe.cs
@@ -0,0 +1,42 @@
+﻿namespace HNI_TPmoyennes;
+
+class Classe
+{
+    public string nomClasse {  get; set; }
+    public List<Eleve> eleves { get; set; } = new List<Eleve>(30);
+    public List<string> matieres { get; set; } = new List<string>(10);
+    public Classe(string _nomClasse)
+    {
+        nomClasse = _nomClasse;
+    }
+
+    public void ajouterEleve(string prenom, string nom)
+    {
+        eleves.Add(new Eleve(prenom, nom));
+    }
+
+    public void ajouterMatiere(string nomMatiere)
+    {
+        matieres.Add(nomMatiere);
+    }
+
+    public float moyenneMatiere(int indice)
+    {
+        float somme = 0f;
+        foreach (Eleve eleve in eleves)
+        {
+            somme += eleve.notes[indice].note;
+        }
+        return (float) Math.Round(somme / this.eleves.Count, 2);
+    }
+
+    public float moyenneGeneral()
+    {
+        float somme = 0f;
+        for(int i=0; i<matieres.Count;++i)
+        {
+            somme += moyenneMatiere(i);
+        }
+        return (float)Math.Round(somme / matieres.Count, 2);
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,33 @@
+﻿namespace HNI_TPmoyennes;
+
+class Eleve
+{
+    public string prenom { get; set; }
+    public string nom {  get; set; }
+    public List<Note> notes { get; set; } = new List<Note>(200);
+    public Eleve(string _prenom, string _nom)
+    {
+        prenom = _prenom;
+        nom = _nom;
+    }
+
+    public void ajouterNote(Note note)
+    {
+        notes.Add(note);
+    }
+
+    public float moyenneMatiere(int indice)
+    {
+        return (float)Math.Round(notes[indice].note, 2);
+    }
+
+    public float moyenneGeneral()
+    {
+        float somme = 0f;
+        foreach (Note note in notes)
+        {
+            somme += note.note;
+        }
+        return (float)Math.Round(somme /notes.Count, 2);
+    }
+}
